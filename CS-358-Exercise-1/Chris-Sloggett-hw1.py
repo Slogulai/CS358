f566893338201ea0a2bb86e17f889d20b1f05d1f
Slogulai <153771171+Slogulai@users.noreply.github.com>
2024-10-03 14:53:01 -0700

    Skeleton program set up for HW 1 and readme made as well

---
+++ CS-358-Exercise-1/Chris-Sloggett-hw1.py
@@ -0,0 +1,52 @@
+# Christopher Sloggett
+# CS 358 Fall 2024
+
+def main():
+    print('Hello, World!')
+
+    
+# Question 1: palindrome1 is a string operation function that checks if a string is a palindrome.
+#            palindrome2 is a loop operation function that checks if a string is a palindome.
+
+def palindrome1(str):
+    pass
+
+def palindrome2(str):
+    pass
+
+# Question 2: Stacks and queues!
+# Create a stack and queue and the associated functions for said data structures:
+# push(s,x) — push x to stack s, return the modified stack
+# pop(s) — pop an item off stack s, return the item
+# enqueue(q,x) — add x to the tail of queue q, return the modified queue
+# dequeue(q) — remove an item from the head of queue q, return the item
+
+def stack():
+    return []
+def queue():
+    return []
+
+def push(s,x):
+    pass
+def pop(s):
+    pass
+def enqueue(q,x):
+    pass
+def dequeue(q):
+    pass
+
+# Question 3: 
+# Write two versions of a factorial function: 
+# fac1(n) – implement with recursion
+# fac2(n) – implement with a loop
+
+def fac1(n):
+    pass
+def fac2(n):
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ CS-358-Exercise-1/README.md
@@ -0,0 +1,5 @@
+# CS-358-Exercise-1
+
+To run this program, will will need the most recent version of python on your machine.
+Download the files then run python on an editor of your choice or from the command line
+type 'python3 Chris-Sloggett-hw1.py' to run the program
