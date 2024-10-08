815749191dfa14f0e47e4a2fef1a6f2085ec149f
Slogulai <153771171+Slogulai@users.noreply.github.com>
2024-10-08 15:15:45 -0700

    Added another example for the parsing

--- CS-358-Exercise-2/Chris-Sloggett-Ex2.py
+++ CS-358-Exercise-2/Chris-Sloggett-Ex2.py
@@ -1,7 +1,21 @@
 # Christopher Sloggett
 # CS 358 Fall 2024
 
-from lark import Lark
+from lark import Lark, v_args
+from lark.visitors import Interpreter
+@v_args(inline=True)
+class Eval(Interpreter):
+    def num(self, val):
+        return int(val)
+    def add(self, left, right):
+        return Eval().visit(left) + Eval().visit(right)
+    def sub(self, left, right):
+        return Eval().visit(left) - Eval().visit(right)
+    def mul(self, left, right):
+        return Eval().visit(left) * Eval().visit(right)
+    def div(self, left, right):
+        return Eval().visit(left) / Eval().visit(right)
+    
 grammar = """
     start: expr
     expr: expr "+" term -> add
@@ -16,10 +30,16 @@ grammar = """
 """  
 parser = Lark(grammar , start='start')
 
+
+
 def main(): 
     prog = "2*3-(1+4)"
     tree = parser.parse(prog)
     print(tree.pretty())
+    
+    prog2 = "2*3-(1+4)"
+    tree2 = parser.parse(prog2)
+    print(tree2.pretty())   
 
 if __name__ == "__main__":
     main()
