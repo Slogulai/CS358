3fb73cad0a3d302e6d55b436e7919c56552c5273
Christopher Sloggett <mrsloggett@gmail.com>
2024-10-22 11:10:32 -0700

    Letex works correctly, on to testing

--- assign1/letex.py
+++ assign1/letex.py
@@ -11,9 +11,21 @@ from lark.visitors import Interpreter
 # 1. Grammar
 #
 grammar = """
-
-    # ... need code
-
+    ?expr0: "let" ID "=" expr0 "in" expr0 -> let
+        | expr
+    ?expr: expr "+" term -> add
+        | expr "-" term -> sub
+        | term
+    ?term: term "*" atom -> mul
+        | term "/" atom -> div
+        | atom
+    ?atom: "(" expr0 ")"
+        | ID -> var
+        | NUM -> num
+    %import common.CNAME -> ID
+    %import common.NUMBER -> NUM
+    %import common.WS
+    %ignore WS 
 """
 
 # Parser
@@ -26,23 +38,24 @@ grammar = """
 #             var x
 #             num 1
 #
-parser = Lark(grammar)
-
+parser = Lark(grammar, start='expr0', parser='lalr')
 
 # 2. Variable environment
 #
 class Env(dict):
     def extend(self,x,v):
-        pass
-        # ...
-
+        if x in self:
+            self[x].insert(0,v)
+        else:
+            self[x] = [v]
     def lookup(self,x): 
-        pass
-        # ... 
-
+        vals = super().get(x)
+        if not vals:
+            raise Exception("Endefined variable: " + x)
+        return vals[0]
     def retract(self,x):
-        pass
-        # ...
+        assert x in self, "Undefined variable: " + x
+        self[x].pop(0)
 
 env = Env()
 
@@ -53,10 +66,33 @@ env = Env()
 #
 @v_args(inline=True)
 class Eval(Interpreter):
-        pass
+    def let(self, name, value_expr, body_expr):
+        value = self.visit(value_expr)
+        env.extend(name, value)
+        result = self.visit(body_expr)
+        env.retract(name)
+        return result
 
-    # ... need code
+    def add(self, left, right):
+        return self.visit(left) + self.visit(right)
 
+    def sub(self, left, right):
+        return self.visit(left) - self.visit(right)
+
+    def mul(self, left, right):
+        return self.visit(left) * self.visit(right)
+
+    def div(self, left, right):
+        return self.visit(left) / self.visit(right)
+
+    def group(self, expr):
+        return self.visit(expr)
+
+    def var(self, name):
+        return env.lookup(name)
+
+    def num(self, value):
+        return int(value)
 
 def main():
     while True:
